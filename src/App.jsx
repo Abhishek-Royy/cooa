7f6c0c60e6ebb5c26577de159e8720a128a222a1
Abhishek-coder2001 <abhishek800.roy@gmail.com>
2025-01-10 12:22:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1>Hello Guys</h1>
+      <a href="/coa.txt"> Section-E</a>
+      <br /><br /><br />
+      <a href="/coa-D.txt"> Section-D</a>
+    </>
+  );
+}
+
+export default App;
