55e383acf0486b2823dcf54f68e998f4a5f2afa5
Abhishek-coder2001 <abhishek800.roy@gmail.com>
2025-01-13 23:01:41 +0530

    new data added

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,47 @@ import React from "react";
 function App() {
   return (
     <>
-      <h1>Hello Guys</h1>
-      <a href="/coa.txt"> Section-E</a>
+      <h1>Hello Guys Mauj Karo</h1>
+      <a href="/binary_Search.txt">Binary Search</a>
       <br /><br /><br />
-      <a href="/coa-D.txt"> Section-D</a>
+      <a href="/bubble_Sort.txt">Bubble Sort</a>
+      <br /><br /><br />
+      <a href="/circular_LinkedList.txt">Display & Create Circular Linked List</a>
+      <br /><br /><br />
+      <a href="/deletion_Circular_LinkedList.txt">Deletion Circular Linked List</a>
+      <br /><br /><br />
+      <a href="/deletion_Doubly_LinkedList.txt">Deletion Doubly Linked List</a>
+      <br /><br /><br />
+      <a href="/deletion_Singly_LinkedList.txt">Deletion Singly Linked List</a>
+      <br /><br /><br />
+      <a href="/dequeue.txt">Dequeue Operation</a>
+      <br /><br /><br />
+      <a href="/doubly_LinkedList.txt">Doubly Linked List Implementation</a>
+      <br /><br /><br />
+      <a href="/enqueue.txt">Enqueue Operation</a>
+      <br /><br /><br />
+      <a href="/insertion_Circular_LinkedList.txt">Insertion Circular Linked List</a>
+      <br /><br /><br />
+      <a href="/insertion_Doubly_LinkedList.txt">Insertion Doubly Linked List</a>
+      <br /><br /><br />
+      <a href="/insertion_Singly_LinkedList.txt">Insertion Singly Linked List</a>
+      <br /><br /><br />
+      <a href="/insertion.txt">Implement Insertion Sort</a>
+      <br /><br /><br />
+      <a href="/linear_search.txt">Implement Linear Search</a>
+      <br /><br /><br />
+      <a href="/merge_sort.txt">Implement Merge Sort</a>
+      <br /><br /><br />
+      <a href="/pop.txt">Pop Operation</a>
+      <br /><br /><br />
+      <a href="/push.txt">Push Operation</a>
+      <br /><br /><br />
+      <a href="/quick_Sort.txt">Implement Quick Sort</a>
+      <br /><br /><br />
+      <a href="/selection_Sort.txt">Implement Selection Sort</a>
+      <br /><br /><br />
+      <a href="/single_LinkedList.txt">Create & Display Singly Linked List</a>
+      <br /><br /><br />
     </>
   );
 }
